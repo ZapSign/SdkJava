825182efb9058beb79d35932a833357c53211342
Eduardo Christino <eduardomaizow@gmail.com>
2022-12-09 10:50:23 -0300

    assinatura em lote

--- src/signers/SignerRequest.java
+++ src/signers/SignerRequest.java
@@ -2,6 +2,7 @@ package signers;
 
 import body.doc.DocFromPdf;
 import body.doc.DocResponse;
+import body.signer.SignBatch;
 import body.signer.Signer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -65,6 +66,22 @@ public class SignerRequest {
                 .uri(URI.create(this.apiRoute+"signer/"+docToken+"/remove/?api_token="+apiToken))
                 .header("Content-Type", "application/json")
                 .method("DELETE", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+    }
+
+    public String signInBatch(String apiToken, SignBatch signBatch) throws IOException, InterruptedException {
+
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonDoc = ow.writeValueAsString(signBatch);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(this.apiRoute+"sign/?api_token="+apiToken))
+                .header("Content-Type", "application/json")
+                .method("POST", HttpRequest.BodyPublishers.ofString(jsonDoc))
                 .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
 
